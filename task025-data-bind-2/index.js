e390bd6890fc06bdbbcb0377b05f7e93a45eb534
cloudseer <cloudseer@163.com>
2017-04-07 16:58:22 +0800

    change data bind 2

--- task025-data-bind-2/index.js
+++ task025-data-bind-2/index.js
@@ -2,7 +2,7 @@
 * @Author: heqingqiu
 * @Date:   2017-04-06 20:10:59
 * @Last Modified by:   cloudseer
-* @Last Modified time: 2017-04-07 16:32:46
+* @Last Modified time: 2017-04-07 16:54:06
 */
 
 'use strict';
@@ -45,17 +45,21 @@ p.convert = function(item, value){
 		configurable: true,
 		get: function(){
 			console.log('我被点啦！我是 ' + item);
-			
+			return value;
 		},
 		set: function(newValue){
 			console.log('小心一点，我要被改了,我是 ' + item);
 			console.log('你把我改成了 ' + newValue);
-			value = newValue;
 			if(item in self.handlers){
 				for(var i = 0; i < self.handlers[item].length; i++){
-					self.handlers[item][i](newValue);
+					self.handlers[item][i](value,newValue);
 				}
 			}
+			value = newValue;
+			if(typeof value === 'object'){
+				new Observer(value);
+			}
+			return value;
 		}
 	})
 }
